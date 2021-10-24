497484f4cb2c09a199ced66fb71d7d80f5b1b299
Alan F <alannfann@gmail.com>
2021-10-23 17:13:23 -0700

    added navbar and headers to pages

--- src/pages/Contribute/index.js
+++ src/pages/Contribute/index.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router';
+import NavBar from '../App/Components/NavBar';
 import './style.css'
 
 export default class NotFound extends Component {
 	render() {
 		return (
 			<div>
-				<h1>Error 404: Page Not Found</h1>
-				<Link to="/">Go to home page</Link>
+				<NavBar></NavBar>
+				<h1>Contribute to a Fund</h1>
 			</div>
 		);
 	}
