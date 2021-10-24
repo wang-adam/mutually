04b3987848c86ce214518c707d9217e65361aba7
Adam <adamw43081@gmail.com>
2021-10-23 22:50:14 -0700

    frontend changes.

--- remora/src/App.js
+++ remora/src/App.js
@@ -7,17 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Remora</h1>
-        <Login />
-        <div id="about">
-          <div
-            style={{ backgroundColor:'rgb(62, 139, 255)', textAlign:'center', padding:'1% 5%', textAlign: 'justify' }}>
+          <div className="about"
+            style={{backgroundColor:'rgb(62, 139, 255)', textAlign:'center', padding:'1% 5%', textAlign: 'justify' }}>
             <p className="title" style={{textAlign:'center'}}>Remora</p>
             <p className="text" style={{textAlign:'left', textIndent: '3vw'}}>
-              Description here
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
-        </div>
+
+        <Login />
+
         <div className="g-signin2" data-onsuccess="onSignIn"></div>
         <div className="row">
           <div className="col-sm-3">
