1e6da9f54492daf15627481a8f946bb2c302a7d5
kavelrao <kavelrao@uw.edu>
2021-10-23 22:08:05 -0700

    created new react app

---
+++ remora/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import Login from './Components/Login';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Remora</h1>
+        <Login />
+        <div id="about">
+          <div
+            style={{ backgroundColor:'rgb(62, 139, 255)', textAlign:'center', padding:'1% 5%', textAlign: 'justify' }}>
+            <p className="title" style={{textAlign:'center'}}>Remora</p>
+            <p className="text" style={{textAlign:'left', textIndent: '3vw'}}>
+              Description here
+            </p>
+          </div>
+        </div>
+        <div className="g-signin2" data-onsuccess="onSignIn"></div>
+        <div className="row">
+          <div className="col-sm-3">
+            <div className="card">
+              <div className="card-body">
+                <h5 className="card-title">Fund 1</h5>
+                <p className="card-text">Fund description</p>
+                <a href="#" className="btn btn-primary">Donate</a>
+                <a href="#" className="btn btn-success">Request</a>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+
+    );
+  }
+}
+
+export default App;
